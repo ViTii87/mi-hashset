26615af2d82ebdc2539d47f91f4d51d25bb74cef
Victor <victormel87@gmail.com>
2016-03-14 12:06:30 +0100

    Commit inicial del proyecto

---
+++ MiHashSet.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class MiHashSet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashSet
+{
+    /**
+     * Constructor for objects of class MiHashSet
+     */
+    public MiHashSet()
+    {
+    
+    }
+
+}
