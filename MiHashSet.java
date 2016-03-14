bbce26b236358233f19b0ea8daded1f9a5c3b792
Victor <victormel87@gmail.com>
2016-03-14 12:38:46 +0100

    Añadido metodo toString a la clase MiHashSet

--- MiHashSet.java
+++ MiHashSet.java
@@ -73,4 +73,22 @@ public class MiHashSet
     public int size(){
         return arrayInt.size();
     }
+    
+    /**
+     * Metodo que devuelve una cadena conteniendo todos los elementos del conjunto 
+     * separados por comas y entre corchetes.
+     */
+    public String toString(){
+        String devuelve = "[";
+        for(int i = 0; i < size(); i++ ){
+            if(i!=size()-1){
+                devuelve += arrayInt.get(i) + ",";
+            }
+            else{
+                devuelve += arrayInt.get(i);
+            }
+        }
+        devuelve += "]";
+        return devuelve;
+    }
 }
